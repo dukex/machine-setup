3d3e27396d07bc252fbb19a4d5653681d8326990
Duke <dukekhaos@gmail.com>
2015-02-16 12:11:04 -0200

    add ubuntu config

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -207,4 +207,6 @@ alias dbmd='spring rake db:migrate:down'
 alias dbmu='spring rake db:migrate:up'
 
 # Homebrew
-alias brewu='brew update  && brew upgrade --all && brew cleanup && brew prune && brew doctor'
+alias brewu='brew update && brew upgrade && brew cleanup && brew prune && brew doctor'
+
+alias open='xdg-open'
