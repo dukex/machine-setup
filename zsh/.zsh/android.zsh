224b70e359c6c889f2a09e26469eb50fed0a7a7e
Duke <dukekhaos@gmail.com>
2017-02-18 12:07:28 -0200

    update android config

--- zsh/.zsh/android.zsh
+++ zsh/.zsh/android.zsh
@@ -1,2 +1,8 @@
-export ANDROID_HOME=/android 
+export ANDROID_HOME=/android
 export PATH="/android/platforms:/android/platform-tools:$PATH"
+
+# export JAVA_HOME=/usr/lib/jvm/default-java
+
+export ANDROID_STUDIO=/android-studio
+export PATH="$ANDROID_STUDIO/bin:$PATH"
+alias studio=$ANDROID_STUDIO/bin/studio.sh
