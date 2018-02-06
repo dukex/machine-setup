8b13af4703fad8595d9486d80f0511d253f7d440
Duke <emerson.almeida@youse.com.br>
2018-02-06 17:14:57 -0200

    use android sdk from AUR folder

--- zsh/.zsh/android.zsh
+++ zsh/.zsh/android.zsh
@@ -1,8 +1,2 @@
-export ANDROID_HOME=/android
-export PATH="/android/platforms:/android/platform-tools:$PATH"
-
-# export JAVA_HOME=/usr/lib/jvm/default-java
-
-export ANDROID_STUDIO=/android-studio
-export PATH="$ANDROID_STUDIO/bin:$PATH"
-alias studio=$ANDROID_STUDIO/bin/studio.sh
+export ANDROID_HOME=/opt/android-sdk
+export PATH="/opt/android-sdk/platforms:/opt/android-sdk/platform-tools:$PATH"
