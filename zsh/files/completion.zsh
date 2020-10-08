f9fd75157e3bf21f62f1faa7fadb6e24d895158a
Emerson Almeida <emersonalmeidax@gmail.com>
2020-10-08 07:37:40 -0300

    improve zsh

---
+++ zsh/files/completion.zsh
@@ -0,0 +1,7 @@
+unsetopt menu_complete   # do not autoselect the first completion entry
+unsetopt flowcontrol
+setopt auto_menu         # show completion menu on successive tab press
+setopt complete_in_word
+setopt always_to_end
+
+autoload -U +X bashcompinit && bashcompinit
