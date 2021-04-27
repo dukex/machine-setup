b7a299363576fa974fc77fd45a59fc1f2c30ad89
Duke <emersonalmeidax@gmail.com>
2021-04-27 17:15:58 -0300

    Update aliases.zsh

--- zsh/files/aliases.zsh
+++ zsh/files/aliases.zsh
@@ -1,16 +1,3 @@
-# PS
-alias psa="ps aux"
-alias psg="ps aux | grep "
-alias psr='ps aux | grep ruby'
-
-# Moving around
-alias cdb='cd -'
-alias cls='clear;ls'
-
-# Show human friendly numbers and colors
-alias df='df -h'
-alias du='du -h -d 2'
-
 alias ll='ls -alh --color=auto'
 alias ls='ls --color=auto'
 
@@ -20,10 +7,6 @@ alias lsg='ll | grep'
 alias gar="killall -HUP -u \"$USER\" zsh"  #global alias reload
 
 alias :q='exit'
-
-alias ve='vim ~/.vimrc'
-
-alias ze='vim ~/.zshrc'
 
 # Git Aliases
 alias gs='git status'
@@ -88,86 +71,3 @@ alias gdmb='git branch --merged | grep -v "\*" | xargs -n 1 git branch -d'
 
 # Common shell functions
 alias less='less -r'
-alias tf='tail -f'
-alias l='less'
-alias lh='ls -alt | head' # see the last modified files
-alias screen='TERM=screen screen'
-alias cl='clear'
-
-# Zippin
-alias gz='tar -zcvf'
-
-# Ruby
-# alias c='rails c' # Rails 3
-# alias co='script/console' # Rails 2
-# alias cod='script/console --debugger'
-
-#If you want your thin to listen on a port for local VM development
-#export VM_IP=10.0.0.1 <-- your vm ip
-# alias ts='thin start -a ${VM_IP:-127.0.0.1}'
-# alias ms='mongrel_rails start'
-alias tfdl='tail -f log/development.log'
-alias tftl='tail -f log/test.log'
-
-alias ka9='killall -9'
-alias k9='kill -9'
-
-# Gem install
-alias sgi='sudo gem install --no-ri --no-rdoc'
-
-# TODOS
-# This uses NValt (NotationalVelocity alt fork) - http://brettterpstra.com/project/nvalt/
-# to find the note called 'todo'
-alias todo='open nvalt://find/todo'
-
-# Forward port 80 to 3000
-alias portforward='sudo ipfw add 1000 forward 127.0.0.1,3000 ip from any to any 80 in'
-
-alias rdm='rake db:migrate'
-alias rdmr='rake db:migrate:redo'
-
-# Zeus
-# alias zs='zeus server'
-# alias zc='zeus console'
-# alias zr='zeus rspec'
-# alias zrc='zeus rails c'
-# alias zrs='zeus rails s'
-# alias zrdbm='zeus rake db:migrate'
-# alias zrdbtp='zeus rake db:test:prepare'
-
-# Rspec
-# alias rs='rspec spec'
-# alias sr='spring rspec'
-# alias src='spring rails c'
-# alias srgm='spring rails g migration'
-# alias srdm='spring rake db:migrate'
-# alias srdt='spring rake db:migrate'
-# alias srdmt='spring rake db:migrate db:test:prepare'
-
-
-# Sprintly - https://github.com/nextbigsoundinc/Sprintly-GitHub
-alias sp='sprintly'
-# spb = sprintly branch - create a branch automatically based on the bug you're working on
-alias spb="git checkout -b \`sp | tail -2 | grep '#' | sed 's/^ //' | sed 's/[^A-Za-z0-9 ]//g' | sed 's/ /-/g' | cut -d"-" -f1,2,3,4,5\`"
-
-alias hpr='hub pull-request'
-alias grb='git recent-branches'
-
-# alias dbtp='spring rake db:test:prepare'
-# alias dbm='spring rake db:migrate'
-# alias dbmr='spring rake db:migrate:redo'
-# alias dbmd='spring rake db:migrate:down'
-# alias dbmu='spring rake db:migrate:up'
-
-alias open='xdg-open'
-
-alias deploy='gpsh & git push heroku master'
-
-
-# Rails / Ruby
-
-alias brr="bin/rspec"
-alias br="bin/rails"
-alias brk="bin/rake"
-alias brc="br c"
-alias brs="br s"
