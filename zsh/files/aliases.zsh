f90cefba8c67ac923ca5694a0b441511c42537f7
Emerson Almeida <emersonalmeidax@gmail.com>
2024-01-31 13:12:16 -0300

    add kube alias

--- zsh/files/aliases.zsh
+++ zsh/files/aliases.zsh
@@ -3,3 +3,5 @@ alias gpsh='git push -u origin `git rev-parse --abbrev-ref HEAD`'
 alias gdmb='git branch --merged | grep -v "\*" | xargs -n 1 git branch -d'
 alias gplr='git pull --rebase'
 alias :q='exit'
+
+[ -f ~/.kubectl_aliases ] && source ~/.kubectl_aliases
