358a445b3a7d34db0928f286a30a817b015d55c3
Emerson Almeida <emersonalmeidax@gmail.com>
2024-11-09 12:40:11 -0300

    improve flutter config

--- editor/files/config.lua
+++ editor/files/config.lua
@@ -26,7 +26,8 @@ lvim.builtin.treesitter.ensure_installed = {
   "scala",
   "kotlin",
   "yaml",
-  "html"
+  "html",
+  "dart"
 }
 
 lvim.builtin.treesitter.ignore_install = { "haskell" }
@@ -39,17 +40,65 @@ formatters.setup {
   { name = "prettier" }
 }
 
+lvim.builtin.which_key.mappings["F"] = {
+  name = "+Flutter",
+  c = { "<cmd>Telescope flutter commands<cr>", "Open Flutter Commands" },
+  d = { "<cmd>FlutterDevices<cr>", "Flutter Devices" },
+  e = { "<cmd>FlutterEmulators<cr>", "Flutter Emulators" },
+  r = { "<cmd>FlutterReload<cr>", "Hot Reload App" },
+  R = { "<cmd>FlutterRestart<cr>", "Hot Restart app" },
+  q = { "<cmd>FlutterQuit<cr>", "Quit running application" },
+  L = { "<cmd>FlutterIntlGenerate<cr>", "Flutter Intl: generate classes" },
+  l = { "<cmd>FlutterIntlDownload<cr>", "Flutter Intl: download .arb" },
+  v = { "<cmd>Telescope flutter fvm<cr>", "Flutter version" },
+}
 
 lvim.plugins = {
+  -- {
+  --   "vhyrro/luarocks.nvim",
+  --   opts = {
+  --     rocks = { "lua-curl", "nvim-nio", "mimetypes", "xml2lua" },
+  --   },
+  -- },
+  { "stevearc/dressing.nvim" },
+  {
+    "j-hui/fidget.nvim",
+    opts = {},
+  },
+  -- {
+  --   'rest-nvim/rest.nvim',
+  --   dependencies = {
+  --     "j-hui/fidget.nvim",
+  --     "nvim-neotest/nvim-nio"
+  --   }
+  -- },
   { "ntpeters/vim-better-whitespace" },
   {
     'akinsho/flutter-tools.nvim',
     lazy = false,
     dependencies = {
       'nvim-lua/plenary.nvim',
-      'stevearc/dressing.nvim', -- optional for vim.ui.select
+      'stevearc/dressing.nvim',
     },
-    config = true,
+    config = function()
+      require("flutter-tools").setup({
+        decorations = {
+          statusline = {
+            app_version = true,
+            device = true,
+          },
+        },
+        dev_tools = {
+          autostart = true,
+          auto_open_browser = true,
+        },
+        lsp = {
+          color = {
+            enabled = false,
+          },
+        },
+      })
+    end,
   },
   {
     "rose-pine/neovim",
@@ -126,6 +175,67 @@ lvim.plugins = {
       -- vim.cmd("colorscheme rose-pine-dawn")
     end
   },
+  {
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    event = "InsertEnter",
+    config = function()
+      require("copilot").setup({})
+    end,
+  },
+  -- {
+  --   "yetone/avante.nvim",
+  --   event = "VeryLazy",
+  --   lazy = false,
+  --   version = false,
+  --   opts = {
+  --     provider = "openai",
+  --     auto_suggestions_provider = "copilot",
+  --     openai = {
+  --       model = "gpt-4o-mini",
+  --     },
+  --   },
+  --   build = "make",
+  --   dependencies = {
+  --     "stevearc/dressing.nvim",
+  --     "nvim-lua/plenary.nvim",
+  --     "MunifTanjim/nui.nvim",
+  --     --- The below dependencies are optional,
+  --     "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+  --     {
+  --       -- support for image pasting
+  --       "HakonHarnes/img-clip.nvim",
+  --       event = "VeryLazy",
+  --       opts = {
+  --         -- recommended settings
+  --         default = {
+  --           embed_image_as_base64 = false,
+  --           prompt_for_file_name = false,
+  --           drag_and_drop = {
+  --             insert_mode = true,
+  --           },
+  --           -- required for Windows users
+  --           use_absolute_path = true,
+  --         },
+  --       },
+  --     },
+  --     {
+  --       -- Make sure to set this up properly if you have lazy=true
+  --       'MeanderingProgrammer/render-markdown.nvim',
+  --       opts = {
+  --         file_types = { "markdown", "Avante" },
+  --       },
+  --       ft = { "markdown", "Avante" },
+  --     },
+  --   },
+  -- }
+  "nvim-dap",
+  {
+    "scalameta/nvim-metals",
+    config = function()
+      require("user.metals").config()
+    end,
+  },
 }
 
 vim.cmd("colorscheme rose-pine")
@@ -145,16 +255,6 @@ vim.filetype.add({
     arb = "json",
   },
 })
-
-lvim.plugins = {
-  "nvim-dap",
-  {
-    "scalameta/nvim-metals",
-    config = function()
-      require("user.metals").config()
-    end,
-  },
-}
 
 local dap = require('dap')
 
@@ -178,3 +278,5 @@ dap.configurations.scala = {
 }
 
 lvim.builtin.dap.active = true
+
+require('lspconfig').dartls.setup {}
